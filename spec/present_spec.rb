a39d62503703fbfc3e45047799b5a3302afd8541
Joe Burgess <josephburgess@gmail.com>
2022-12-05 20:47:54 +0000

    refactoring and adding unwrap

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,16 +1,23 @@
 require 'present'
 
-RSpec.describe 'presents' do
-  context 'returns and error if contents is already wrapped' do
-    it 'fails' do
-      present = Present.new
-      present.wrap('Stuff')
-      expect { present.wrap('Stuff') }.to raise_error 'A contents has already been wrapped.'
+RSpec.describe Present do
+  present = Present.new
+
+  context 'when already wrapped' do
+    it 'fails to wrap' do
+      present.wrap('Socks')
+      expect { present.wrap('Lynx Africa') }.to raise_error 'A contents has already been wrapped.'
     end
   end
-  context 'returns error if trying to unwrap nothing'
-  it 'fails' do
-    present = Present.new
+
+  context 'when not yet wrapped'
+  it 'fails to unwrap' do
     expect { present.unwrap }.to raise_error 'No contents have been wrapped.'
+  end
+
+  context 'when wrapped' do
+    it 'should be able to unwrap' do
+      expect(present.unwrap).to eq 'Socks'
+    end
   end
 end
