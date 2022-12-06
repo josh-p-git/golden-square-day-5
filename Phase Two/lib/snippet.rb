0a91080e5e12cad2dc66380a97831e437366b0b5
Joe Burgess <josephburgess@gmail.com>
2022-12-06 12:07:10 +0000

    adding edge case for non-string input

--- Phase Two/lib/snippet.rb
+++ Phase Two/lib/snippet.rb
@@ -1,5 +1,7 @@
 def snippet(string)
+  return 'Please input a string!' if string.class != String
+
   return string if string.split.length <= 5
-  
+
   "#{string.split.first(5).join(' ')} ..."
 end
--- Phase Two/spec/snippet_spec.rb
+++ Phase Two/spec/snippet_spec.rb
@@ -7,4 +7,7 @@ describe 'snippet' do
   it 'returns only the first 5 words with ... when string is longer than 5 words' do
     expect(snippet('This is a string longer than five words')).to eq 'This is a string longer ...'
   end
+  it 'returns an error message when parameter is not a string' do
+    expect(snippet(9)).to eq 'Please input a string!'
+  end
 end
